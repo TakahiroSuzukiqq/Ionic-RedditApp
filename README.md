87c4404fb23dd89f5232cf30c7edf27b62a672a7
Tucker <takahirosuzuki.m010@gmail.com>
2017-07-30 00:35:44 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# problem I met  
+* [Error: Uncaught (in promise): Error: No provider for Http!](https://forum.ionicframework.com/t/no-provider-for-http-error-in-ionic/85762/2)  
---
+++ src/pages/reddits/reddits.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { RedditService } from '../../app/services/reddit.service';
+
+@Component({
+  selector: 'reddits',
+  templateUrl: 'reddits.html'
+})
+export class RedditsPage {
+
+  constructor(public navCtrl: NavController, private redditService: RedditService) {
+
+  }
+
+  ngOnIt(){  //lifecycle hook
+    console.log('onInit ran...');
+    // this.getPosts('sports', 5);
+  }
+
+  // getPosts(category, limit) {
+  //   this.redditService.getPosts(category, limit).subscribe(response => {
+  //     console.log(response);
+  //   });
+  // }
+}
+
+//memo
+//what is "lifecycle hook"japanese article : http://blog.yuhiisk.com/archive/2016/05/02/angular2-lifecycle-hooks.html
